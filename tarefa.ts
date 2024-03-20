a377de73c87c6396df95f891b047b6e8a6a07456
Roger Santos <rogersw06@gmail.com>
2024-03-20 11:38:07 -0300

    Add files via upload

---
+++ tarefa.ts
@@ -0,0 +1,10 @@
+function multiplicacao(primeiroNumero: number, segundoNumero: number): number {
+    return primeiroNumero * segundoNumero;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicacao(3, 4));
+console.log(saudacao("Roger"));
